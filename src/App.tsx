47a2343e1feac56c5918c239c634fa296bfcb1be
yesharry <reerohh@gmail.com>
2023-06-10 10:45:19 +0900

    Add: components & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
